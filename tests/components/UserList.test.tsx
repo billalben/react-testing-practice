60c5b3d83a9a7e4fdd307a12fcdef5c0af16a2a2
Benzazoua <billalben252@gmail.com>
2025-05-23 15:42:44 +0100

    Testing lists

---
+++ tests/components/UserList.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+
+import UserList from "../../src/components/UserList";
+import { User } from "../../src/entities";
+
+describe("UserAccount", () => {
+  it("should render no users when the users array is empty", () => {
+    render(<UserList users={[]} />);
+    const noUsersMessage = screen.getByText(/no users/i);
+    expect(noUsersMessage).toBeInTheDocument();
+  });
+
+  it("should render a list of users", () => {
+    const users: User[] = [
+      { id: 1, name: "John Doe", isAdmin: false },
+      { id: 2, name: "Jane Smith", isAdmin: true },
+    ];
+
+    render(<UserList users={users} />);
+
+    const userListItems = screen.getAllByRole("listitem");
+    expect(userListItems).toHaveLength(users.length);
+
+    users.forEach((user) => {
+      // const userLink = screen.getByText(user.name);
+      // expect(userLink).toBeInTheDocument();
+      // expect(userLink.closest("a")).toHaveAttribute("href", `/users/${user.id}`);
+
+      const link = screen.getByRole("link", { name: user.name });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
+    });
+  });
+});
