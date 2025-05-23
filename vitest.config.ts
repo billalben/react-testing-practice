3ce22d996f8020c2e5190b5a2486bdf44a32d017
Benzazoua <billalben252@gmail.com>
2025-05-23 14:32:02 +0100

    Simlifying the test setup for Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom",
+    environment: "jsdom", // use jsdom environment for testing
+    globals: true, // use global variables like describe, it, expect
+    setupFiles: "./tests/setup.ts", // this setup runs before each test file
   },
 });
