c78cc901061d5aa35ab8792d6603af43d1c6761b
Benzazoua <billalben252@gmail.com>
2025-06-01 17:50:10 +0100

    Refactor: using React Query

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -7,9 +7,27 @@ import ProductList from "../../src/components/ProductList";
 import { server } from "../mocks/server";
 import { http, HttpResponse, delay } from "msw";
 import { db } from "../mocks/db";
+import { QueryClient, QueryClientProvider } from "react-query";
 // import delay from "delay";
 
 describe("ProductList", () => {
+  const renderComponent = () => {
+    const queryClient = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false, // Disable retries for tests
+          refetchOnWindowFocus: false, // Disable refetching on window focus
+        },
+      },
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <ProductList />
+      </QueryClientProvider>
+    );
+  };
+
   const productIds: number[] = [];
 
   beforeAll(() => {
@@ -24,7 +42,8 @@ describe("ProductList", () => {
   });
 
   it("should render the list of products", async () => {
-    render(<ProductList />);
+    // render(<ProductList />);
+    renderComponent();
 
     const items = await screen.findAllByRole("listitem");
     expect(items.length).toBeGreaterThan(0);
@@ -33,7 +52,8 @@ describe("ProductList", () => {
   it("should render no products available if no product is found", async () => {
     // Override the /products endpoint to return an empty array
     server.use(http.get("/products", () => HttpResponse.json([])));
-    render(<ProductList />);
+    // render(<ProductList />);
+    renderComponent();
 
     const noProductsMessage = await screen.findByText(/No products/i);
     expect(noProductsMessage).toBeInTheDocument();
@@ -43,7 +63,8 @@ describe("ProductList", () => {
     server.use(
       http.get("/products", () => HttpResponse.json(null, { status: 500 }))
     );
-    render(<ProductList />);
+    // render(<ProductList />);
+    renderComponent();
 
     const errorMessage = await screen.findByText(/error/i);
     expect(errorMessage).toBeInTheDocument();
@@ -59,24 +80,27 @@ describe("ProductList", () => {
       })
     );
 
-    render(<ProductList />);
+    // render(<ProductList />);
+    renderComponent();
 
     const loadingMessage = await screen.findByText(/loading/i);
     expect(loadingMessage).toBeInTheDocument();
   });
 
   it("should remove loading state after fetching products", async () => {
-    render(<ProductList />);
+    // render(<ProductList />);
+    renderComponent();
 
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
   });
 
   it("should remove loading state if data fetching failed", async () => {
     server.use(http.get("/products", () => HttpResponse.error()));
 
-    render(<ProductList />);
+    // render(<ProductList />);
+    renderComponent();
 
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
-    await screen.findByText(/error/i);
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
   });
 });
